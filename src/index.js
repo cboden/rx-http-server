5db293730e7c20210495f0171cd97f78895738ff
Chris Boden <cboden@gmail.com>
2016-05-04 11:21:10 -0400

    Use newer version of Rx, use fromEvent operator

--- src/index.js
+++ src/index.js
@@ -5,32 +5,32 @@ var util = require("util");
 function Server() {
     http.Server.call(this);
 
-    this.requests = Rx.Node
+    this.requests = Rx.Observable
         .fromEvent(this, "request", function(args) {
             return { request: args[0], response: args[1] };
         });
 
-    this.closes = Rx.Node
+    this.closes = Rx.Observable
         .fromEvent(this, "close", function(args) {
             return true;
         });
 
-    this.checkContinues = Rx.Node
+    this.checkContinues = Rx.Observable
         .fromEvent(this, "checkContinue", function(args) {
             return { request: args[0], response: args[1] };
         });
 
-    this.connects = Rx.Node
+    this.connects = Rx.Observable
         .fromEvent(this, "connect", function(args) {
             return { request: args[0], socket: args[1], head: args[2] };
         });
 
-    this.upgrades = Rx.Node
+    this.upgrades = Rx.Observable
         .fromEvent(this, "upgrade", function(args) {
             return { request: args[0], socket: args[1], head: args[2] };
         });
 
-    this.clientErrors = Rx.Node
+    this.clientErrors = Rx.Observable
         .fromEvent(this, "clientError", function(args) {
             return { exception: args[0], socket: args[1] };
         });
